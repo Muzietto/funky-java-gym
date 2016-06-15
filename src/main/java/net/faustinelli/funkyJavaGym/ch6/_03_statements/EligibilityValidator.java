b9ee254f958e1db8e5dba8d7249c084c16ba0eda
Muzietto <marco_faustinelli@yahoo.com>
2016-06-15 21:33:33 +0100

    abstracting out.....

--- src/main/java/net/faustinelli/funkyJavaGym/ch6/_03_statements/EligibilityValidator.java
+++ src/main/java/net/faustinelli/funkyJavaGym/ch6/_03_statements/EligibilityValidator.java
@@ -8,52 +8,18 @@
 
 package net.faustinelli.funkyJavaGym.ch6._03_statements;
 
-import net.faustinelli.funkyJavaGym.ch6._03_statements.model.Identifier;
 import net.faustinelli.funkyJavaGym.ch6._03_statements.model.Task;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Predicate;
-
 /**
- * Created by Marco Faustinelli (Muzietto) on 19/05/2016.
+ * Created by Marco Faustinelli (Muzietto) on 15/06/2016.
  */
-public class EligibilityValidator {
+public interface EligibilityValidator<T> {
 
-    private static final String MSS_DEPOT = "MSS_DEPOT";
-    private static final String MSAPL_DEPOT = "MSAPL_DEPOT";
-    private static final String BOOKING_ENGINE = "BOOKING_ENGINE";
-    private static final String FORWARD_BRIDGE = "FORWARD_BRIDGE";
-    private static final String BACKWARD_BRIDGE = "BACKWARD_BRIDGE";
+    static final String MSS_DEPOT = "MSS_DEPOT";
+    static final String MSAPL_DEPOT = "MSAPL_DEPOT";
+    static final String BOOKING_ENGINE = "BOOKING_ENGINE";
+    static final String FORWARD_BRIDGE = "FORWARD_BRIDGE";
+    static final String BACKWARD_BRIDGE = "BACKWARD_BRIDGE";
 
-    public boolean isEligible(Task task) {
-    /*        
-        String depot = task.getDepot();
-        return MSS_DEPOT.equals(depot) || MSAPL_DEPOT.equals(depot)
-           || (task.getId() != null && (
-                BOOKING_ENGINE.equals(task.getId().getSystem())
-                || BACKWARD_BRIDGE.equals(task.getId().getSystem())
-                || FORWARD_BRIDGE.equals(task.getId().getSystem())
-        ));
-    */
-
-        Optional<String> depot = Optional.ofNullable(task.getDepot());
-        Optional<String> sourceSystemCode = Optional.ofNullable(task.getId())
-            .map(Task.TriggerId::getSystem)
-                .map(Task.SourceSystem::getCode);
-
-        Boolean isValidDepot = new Statements<String>(
-            MSS_DEPOT::equals,
-            MSAPL_DEPOT::equals
-        ).disjunction(depot);
-
-        Boolean isValidSystem = new Statements<String>(
-            BOOKING_ENGINE::equals,
-            BACKWARD_BRIDGE::equals,
-            FORWARD_BRIDGE::equals
-        ).disjunction(sourceSystemCode);
-
-        return isValidDepot || isValidSystem;
-    }
+    Boolean isEligible(T task);
 }
